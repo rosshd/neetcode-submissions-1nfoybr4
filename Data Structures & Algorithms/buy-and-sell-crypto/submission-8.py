19a2cd57caa2734221ea9e586eb71ef0aaabc0eb
Ross Dawson <rosshdawson@gmail.com>
2026-04-24 14:24:45 -0400

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        most = 0
+        l, r = 0, 1
+        
+        while r < len(prices):
+            if prices[r] > prices[l]:
+                most = max(most, prices[r] - prices[l])
+            else:
+                l = r
+            r += 1
+                
+        return most
+            
