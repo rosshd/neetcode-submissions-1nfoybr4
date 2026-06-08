7dbc17a998190048d971402a2a8c4ccda8849c8a
rosshd <rosshdawson@gmail.com>
2026-06-07 22:52:08 -0400

    Add solution:

---
+++ leetcode.nvim/125.valid-palindrome.py
@@ -0,0 +1,53 @@
+# @leet imports start
+from string import *
+from re import *
+from datetime import *
+from collections import *
+from heapq import *
+from bisect import *
+from copy import *
+from math import *
+from random import *
+from statistics import *
+from itertools import *
+from functools import *
+from operator import *
+from io import *
+from sys import *
+from json import *
+from builtins import *
+import string
+import re
+import datetime
+import collections
+import heapq
+import bisect
+import copy
+import math
+import random
+import statistics
+import itertools
+import functools
+import operator
+import io
+import sys
+import json
+from typing import *
+# @leet imports end
+
+# @leet start
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while !s[l].isalnum():
+                l+=1
+            while !s[r].isalnum():
+                r-=1
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+# @leet end
