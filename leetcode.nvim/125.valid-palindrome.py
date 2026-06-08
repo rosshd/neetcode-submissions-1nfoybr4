952c66ab509e12db3894721508a7554dc9e3e4f5
rosshd <rosshdawson@gmail.com>
2026-06-07 23:13:08 -0400

    Add solution:

--- leetcode.nvim/125.valid-palindrome.py
+++ leetcode.nvim/125.valid-palindrome.py
@@ -35,19 +35,22 @@ import json
 from typing import *
 # @leet imports end
 
+
 # @leet start
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         l, r = 0, len(s) - 1
 
         while l < r:
-            while !s[l].isalnum():
-                l+=1
-            while !s[r].isalnum():
-                r-=1
-            if s[l] != s[r]:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
                 return False
             l += 1
             r -= 1
         return True
+
+
 # @leet end
