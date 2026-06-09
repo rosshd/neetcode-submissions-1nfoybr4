7c5aaa5d9f03e94baaa0b273b942274a41f4733f
rosshd <rosshdawson@gmail.com>
2026-06-09 18:35:52 -0400

    Add solution:

---
+++ leetcode.nvim/3.longest-substring-without-repeating-characters.py
@@ -0,0 +1,54 @@
+# @leet imports start
+from string import *
+from re import *
+from datetime import *
+from collections import *
+from heapq import *
+from bisect import *
+from copy import *
+from math import *
+from random import *
+from statistics import *
+from itertools import *
+from functools import *
+from operator import *
+from io import *
+from sys import *
+from json import *
+from builtins import *
+import string
+import re
+import datetime
+import collections
+import heapq
+import bisect
+import copy
+import math
+import random
+import statistics
+import itertools
+import functools
+import operator
+import io
+import sys
+import json
+from typing import *
+# @leet imports end
+
+
+# @leet start
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = set()
+        longest, l = 0, 0
+        for r in range(len(s)):
+            while s[r] in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(s[r])
+            longest = max(longest, r - l + 1)
+        return longest
+
+
+# @leet end
+
