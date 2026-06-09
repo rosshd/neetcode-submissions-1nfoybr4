7e7882e8e07bb70a2084274bffc22e3d0282289e
rosshd <rosshdawson@gmail.com>
2026-06-08 22:21:31 -0400

    Add solution:

---
+++ leetcode.nvim/16.3sum-closest.py
@@ -0,0 +1,58 @@
+# @leet imports start
+from string import *
+from re import *
+from datetime import *
+from collections import *
+from heapq import *
+from bisect import *
+from copy import *
+from math import *
+from random import *
+from statistics import *
+from itertools import *
+from functools import *
+from operator import *
+from io import *
+from sys import *
+from json import *
+from builtins import *
+import string
+import re
+import datetime
+import collections
+import heapq
+import bisect
+import copy
+import math
+import random
+import statistics
+import itertools
+import functools
+import operator
+import io
+import sys
+import json
+from typing import *
+# @leet imports end
+
+
+# @leet start
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        closest = float(inf)
+        nums.sort()
+        for slow in range(len(nums) - 2):
+            l, r = slow + 1, len(nums) - 1
+            while l < r:
+                total = nums[slow] + nums[l] + nums[r]
+                if abs(target - total) < abs(closest):
+                    closest = total
+                if target - total > 0:
+                    r -= 1
+                else:
+                    l += 1
+        return closest
+
+
+# @leet end
+
